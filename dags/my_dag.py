5e1607875281ea626239d9f4bdffe3f2c636232e
Vinoth <vinothkumar_r_external@bat.com>
2024-07-18 14:50:43 +0530

    Update my_dag.py

--- dags/my_dag.py
+++ dags/my_dag.py
@@ -1,16 +1,14 @@
-######
-
 import os
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.dummy import DummyOperator
-from airflow.contrib.operators.bigquery_operator import BigqueryOperator
-from airflow.contrib.operators.gcs_to_bq import GoogleCloudStorageToBigqueryOperator
+from airflow.contrib.operators.bigquery_operator import BigQueryOperator
+from airflow.contrib.operators.gcs_to_bq import GoogleCloudStorageToBigQueryOperator
 
-yesterday = datetime.combinr(datetime.today() - timedelta(1), datetime.min.time())
+yesterday = datetime.combine(datetime.today() - timedelta(1), datetime.min.time())
 
-default_args={
-    'satrt_date':yesterday,
+default_args = {
+    'start_date': yesterday,
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
@@ -24,37 +22,36 @@ with DAG(
     default_args=default_args
 ) as dag:
 
-    #taskoperator
-    
-    start=DummyOperator(
+    # Task: start
+    start = DummyOperator(
         task_id='start',
         dag=dag,
     )
 
-    #gcstobq
-    gcs_to_bq=GoogleCloudStorageToBigqueryOperator(
+    # Task: gcs_to_bq
+    gcs_to_bq = GoogleCloudStorageToBigQueryOperator(
         task_id='gcs_to_bq',
         bucket='data_eng_demos',
         source_objects=['Alubee_Update.xlsx'],
         destination_project_dataset_table='data-eng-demos19.gcp_dataeng_demos.gcs_to_bq_table',
         schema_fields=[
-                        {'name': 'Project Health', 'type': 'STRING', 'mode': 'NULLABLE'},
-                        {'name': 'Allocated Resource', 'type': 'INT', 'mode': 'NULLABLE'},
-                        {'name': 'Project Lead', 'type': 'STRING', 'mode': 'NULLABLE'},
-                        {'name': 'Developers', 'type': 'STRING', 'mode': 'NULLABLE'},
-                        {'name': 'Client Feedback', 'type': 'STRING', 'mode': 'NULLABLE'},
-                        {'name': 'Project Insight & Deliverables', 'type': 'STRING', 'mode': 'NULLABLE'},
-                        {'name': 'Risk/Blockers', 'type': 'STRING', 'mode': 'NULLABLE'},
-                        {'name': 'New Requirements', 'type': 'STRING', 'mode': 'NULLABLE'}
-                        ],
+            {'name': 'Project Health', 'type': 'STRING', 'mode': 'NULLABLE'},
+            {'name': 'Allocated Resource', 'type': 'INT', 'mode': 'NULLABLE'},
+            {'name': 'Project Lead', 'type': 'STRING', 'mode': 'NULLABLE'},
+            {'name': 'Developers', 'type': 'STRING', 'mode': 'NULLABLE'},
+            {'name': 'Client Feedback', 'type': 'STRING', 'mode': 'NULLABLE'},
+            {'name': 'Project Insight & Deliverables', 'type': 'STRING', 'mode': 'NULLABLE'},
+            {'name': 'Risk/Blockers', 'type': 'STRING', 'mode': 'NULLABLE'},
+            {'name': 'New Requirements', 'type': 'STRING', 'mode': 'NULLABLE'}
+        ],
         skip_leading_rows=1,
         create_disposition='CREATE_IF_NEEDED',
         write_disposition='WRITE_TRUNCATE',
         dag=dag
     )
 
-    #bigquery_manipulation
-    bq_manipulation=BigqueryOperator(
+    # Task: bq_manipulation
+    bq_manipulation = BigQueryOperator(
         task_id='bq_manipulation',
         use_legacy_sql=False,
         allow_large_results=True,
@@ -72,13 +69,13 @@ with DAG(
                     variable,\
                     source",
         dag=dag
-
     )
-    #end
-    end=DummyOperator(
+
+    # Task: end
+    end = DummyOperator(
         task_id='end',
         dag=dag,
     )
 
-    #task dependency
+    # Task Dependency
     start >> gcs_to_bq >> bq_manipulation >> end
